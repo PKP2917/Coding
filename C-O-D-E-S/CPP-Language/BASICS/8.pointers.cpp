05a98522c4b5c126a6330acb3739ad5fc05e6184
Pranav <pkkusum4@gmail.com>
2024-02-05 11:39:54 +0530

    commit

--- C-O-D-E-S/CPP-Language/BASICS/8.pointers.cpp
+++ C-O-D-E-S/CPP-Language/BASICS/8.pointers.cpp
@@ -28,3 +28,64 @@ int main(){
     return 0;
 }
 */ 
+
+/*
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=17;
+
+    //int *p;      -> this is bad practice, it will take garbage value
+    int *p = 0;     //-> instead initialise it with 0
+    p = &i;
+
+    cout<<p<<endl;
+    cout<<*p<<endl;
+    
+    
+    return 0;
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num = 17;
+    int a = num;
+    a++;
+
+    cout<<num<<endl;
+
+    int *p = &num;
+    cout<<"Before : "<<num<<endl;
+    (*p)++;
+    cout<<"After : "<<num<<endl;
+
+    return 0;
+}
+*/
+
+
+//-> Pointer to Pointer
+/*
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 17;
+    int *b = &a;
+    int **c = &b;
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<*b<<endl;
+    cout<<*c<<endl;
+    cout<<**c<<endl;
+
+    return 0;
+}
+*/
